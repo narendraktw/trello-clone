5c6157e42eb640915ac41378c4f2e119382da577
narendraktw <narendraktw@gmail.com>
2022-09-06 17:10:28 +0530

    feature: add list item and modal fix

--- src/components/AddListItem.tsx
+++ src/components/AddListItem.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useState } from 'react';
+import Modalbox from './modalbox/Modalbox';
 
 interface AddItemProps {
 	onAdd(text: string): void;
@@ -6,9 +7,42 @@ interface AddItemProps {
 }
 
 const AddListItem = ({ onAdd, toggleButtonText }: AddItemProps) => {
+	const [showModal, setShowModal] = useState(false);
+	const [label, setLabel] = useState('');
+
+	//Add column item
+	const addItem = () => {
+		onAdd(label);
+		setShowModal(false);
+		setLabel('');
+	};
+
 	return (
 		<>
-			<button>{toggleButtonText}</button>
+			<button className="add-btn" onClick={() => setShowModal(true)}>
+				{toggleButtonText}
+			</button>
+			{showModal && (
+				<Modalbox>
+					<div className="modal-header">
+						<div className="modal-title">Add Item</div>
+						<span className="close" onClick={() => setShowModal(false)}>
+							&times;
+						</span>
+					</div>
+
+					<div className="modal-body">
+						<input
+							type="text"
+							autoFocus
+							value={label}
+							onChange={(e) => setLabel(e.target.value)}
+							placeholder="Add Label Name"
+						/>
+						<input type="button" value="Save" onClick={() => addItem()} />
+					</div>
+				</Modalbox>
+			)}
 		</>
 	);
 };
